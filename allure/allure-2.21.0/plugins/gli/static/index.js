d0db4f3c59826175aaad7ce2f619a25e8cdb8229
Yurii Ivanskyi <yurii.ivanskyi@visiquate.com>
2023-02-10 23:26:47 +0200

    It works, it's alive

--- allure/allure-2.21.0/plugins/gli/static/index.js
+++ allure/allure-2.21.0/plugins/gli/static/index.js
@@ -1,65 +1,60 @@
 const template = `
-<h2 class="widget__title">{{title}}</h2>
+<h2 class="widget__title">⚠️{{ title }}</h2>
 {{#if items}}
 <div class="table  table_hover widget__table">
     {{#each items}}
-    <div class="{{b " table" "row" }}" disabled>
-        <div class="{{b " table" "col" }} long-line">
-            {{name}}
+    <div class="table__row" disabled>
+        <div class="table__col long-line">
+            🌍{{name}}
         </div>
-        <div class="{{b " table" "col" }} long-line">
-            {{#each value}}
-            {{linky this}}{{#unless @last}},<br>{{/unless}}
-            {{/each}}
+        {{#if display_value}}
+        <div class="table__col long-line">
+            {{display_value}}
+        </div>
+        {{else}}
+        <div class="table__col long-line">
+            {{value}}
+        </div>
+        {{/if}}
+        <div class="table__col long-line">
+            {{decoration}}
         </div>
     </div>
     {{/each}}
     {{#if overLimit}}
-    <a class="{{b " table" "row" }} clickable environment-widget__expand">
-        <div class="{{b " table" "col" center=true}}">{{widget.globalInfo.showAll}}</div>
+    <a class="table__row clickable">
+        <div class="table__col center">{{ widget.globalInfo.showAll }}</div>
     </a>
     {{/if}}
 </div>
-
 {{else}}
-<div class="widget__noitems">{{widget.globalInfo.empty}}</div>
+<div class="widget__noitems">{{ widget.globalInfo.empty }}</div>
 {{/if}}
 `
 
-allure.api.addTranslation('en', {
-    widget: {
-        globalInfo: {
-            name: 'Global Info',
-            showAll: 'Show All',
-            emty: 'Empty'
-        }
-    }
-});
-
-allure.api.addWidget('widgets', 'global-info', Backbone.Marionette.View.extend({
+class GlobalInfoWidget extends Backbone.Marionette.View {
     template(data) {
-        compiled = Handlebars.compile(template);
-        console.log(compiled(data));
-        return compiled(data)
-    },
-
-    // @on("click .environment-widget__expand")
-    // onExpandClick() {
-    //     this.listLimit = this.model.get("items").length;
-    //     this.render();
-    // },
-    onRender: function () {
-        console.log(this);
-    },
+        return Handlebars.compile(template)(data)
+    }
     initialize() {
         this.listLimit = 15;
-    },
+    }
     serializeData() {
         const items = this.model.get("items");
         return {
             items: items.slice(0, this.listLimit),
             overLimit: items.length > this.listLimit,
-            title: 'Global Info'
+            title: 'Global Warnings & Info'
         };
     }
-}));
+}
+allure.api.addWidget('widgets', 'global-info', GlobalInfoWidget);
+allure.api.addTranslation('en', {
+    widget: {
+        globalInfo: {
+            name: 'Global Info',
+            showAll: 'Show All',
+            empty: 'Empty'
+        }
+    }
+});
