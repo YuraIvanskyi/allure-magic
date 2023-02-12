83e4db4fd80d2957cffb62652b18d6e55a2a17b2
Yura Ivanskyi <yuraivanskyi@gmail.com>
2023-02-12 11:24:51 +0200

    The real working version

--- global-info/src/main/java/i1skyi/GlobalInfoItem.java
+++ global-info/src/main/java/i1skyi/GlobalInfoItem.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonInclude(JsonInclude.Include.ALWAYS)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "name",
         "value",
--- global-info/src/main/java/i1skyi/GlobalInfoWidget.java
+++ global-info/src/main/java/i1skyi/GlobalInfoWidget.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -47,7 +46,6 @@ public class GlobalInfoWidget extends CommonJsonAggregator implements Reader {
                 final List<GlobalInfoItem> info = context.getValue()
                         .readerFor(new TypeReference<List<GlobalInfoItem>>() {
                         }).readValue(is);
-                System.out.println(info);
                 visitor.visitExtra(GLOBAL_INFO, info);
             } catch (IOException e) {
                 visitor.error("Could not read global-info file " + globalInfoFile, e);
@@ -55,44 +53,16 @@ public class GlobalInfoWidget extends CommonJsonAggregator implements Reader {
         }
     }
 
-    public static <T> List<T> flatten(List<List<T>> listOfLists) {
-        return listOfLists.stream().reduce((x, y) -> {
-            List<T> list = new ArrayList<>(x);
-            list.addAll(y);
-            return list;
-        }).orElse(new ArrayList<>());
-    }
-
     @Override
     public List<GlobalInfoItem> getData(final List<LaunchResults> launches) {
-        // final List<Map.Entry<String, String>> warnings = launches.stream()
-        // .flatMap(launch -> launch.getExtra(GLOBAL_INFO,
-        // (Supplier<Map<String, String>>) LinkedHashMap::new).entrySet().stream())
-        // .collect(toList());
-        // System.out.println(Arrays.toString(launches.toArray()));
-        // System.out.println(Arrays.toString(warnings.toArray()));
-        // GlobalInfoItem someObj = new GlobalInfoItem("text_value", "test_name", "some
-        // text", "plain");
-        // GlobalInfoItem someObj2 = new
-        // GlobalInfoItem("https://github.com/YuraIvanskyi", "git", "My GitHub",
-        // "link");
-        // GlobalInfoItem someObj3 = new GlobalInfoItem("<code>let code =
-        // 'code'</code>", "raw_entry", "", "raw");
-        // List<GlobalInfoItem> objList = new ArrayList<GlobalInfoItem>();
-        // objList.add(someObj);
-        // objList.add(someObj2);
-        // objList.add(someObj3);
-        // return objList;
-        List<Object> all = launches.stream()
-                .map(launchResults -> launchResults.getExtra(GLOBAL_INFO))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toList());
-        System.out.println(all);
+        // this is some Java bullshit but it works
         return launches.stream()
                 .map(launchResults -> launchResults.getExtra(GLOBAL_INFO))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
+                .collect(Collectors.toList())
+                .stream()
+                .flatMap(c -> ((Collection<GlobalInfoItem>) c).stream())
                 .filter(GlobalInfoItem.class::isInstance)
                 .map(GlobalInfoItem.class::cast)
                 .collect(Collectors.toList());
