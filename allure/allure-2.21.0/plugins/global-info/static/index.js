80272de379c302bd27ec0d5e67759f729572ce30
Yura Ivanskyi <yuraivanskyi@gmail.com>
2023-02-12 14:41:20 +0200

    Bye, handlebars!

--- allure/allure-2.21.0/plugins/global-info/static/index.js
+++ allure/allure-2.21.0/plugins/global-info/static/index.js
@@ -1,58 +1,62 @@
-const template = `
-<h2 class="widget__title">{{ title }}
-<span class="widget__subtitle">{{items.length}} items total</span>
-</h2>
-<div class="table table_hover widget__table">
-{{#if items}}
-    {{#each items}}
-    <div class="table__row" disabled>
-        <div class="table__col long-line shorter_col">
-            {{name}}
-        </div>
-        {{#if display_value}}
-        <div class="table__col long-line">
-            <a class="link" href="{{value}}">{{display_value}}</a>
-        </div>
-        {{else}}
-        <div class="table__col long-line">
-            {{value}}
-        </div>
-        {{/if}}
-    </div>
-    {{/each}}
-    {{#if overLimit}}
-    <a class="table__row clickable">
-        <div class="table__col table__col_center"> Show All </div>
-    </a>
-    {{/if}}
-{{else}}
-    <div class="table__row">
-        <div class="table__col table__col_center">
-            No messages, warnings or gloabl errors
-        </div>
-    </div>
-{{/if}}
-</div>
-
-`
-
 class WidgetTemplate {
 
     constructor(data) {
-        this.content = data
-        this.template = ``
+        this.content = data;
+        this.template = ``;
     }
+
 
     build() {
-        return this.template.concat(
-            this.title(this.content.title),
-            this.bodyTable()
+        for (const entry of this.content.items) {
+            switch (entry.decoration) {
+                case 'link':
+                    this.template +=
+                        this.entryBase(
+                            this.stack(
+                                this.entryName(entry.name),
+                                this.linkEntryValue(entry.display_value, entry.value)
+                            )
+                        );
+                    break;
+                case 'code':
+                    this.template +=
+                        this.entryBase(
+                            this.stack(
+                                this.entryName(entry.name),
+                                this.codeEntryValue(entry.value)
+                            )
+                        );
+                    break;
+                default:
+                    this.template +=
+                        this.entryBase(
+                            this.stack(
+                                this.entryName(entry.name),
+                                this.entryValue(entry.value)
+                            )
+                        );
+            }
+
+        }
+
+        return this.stack(
+            this.title(this.content.title, this.content.subtitle),
+            this.bodyTable(
+                this.optional(
+                    this.template,
+                    this.entryNoValues('No messages, warnings or gloabl errors'),
+                    this.content.items
+                )
+            ),
         )
     }
-
+    stack = (...args) => args.join('')
+    optional = (value, alternative, condition = true) => condition ? value : alternative
     bodyTable = (innerHtml) => `<div class="table table_hover widget__table">${innerHtml}</div >`
+    title = (title, subtitle) => subtitle ?
+        `<h2 class="widget__title" >${title} <span class="widget__subtitle"> ${subtitle}</span></h2>`
+        : `<h2 class="widget__title" >${title}</h2>`
     entryBase = (entryHtml) => `<div class="table__row" disabled>${entryHtml}</div>`
-    optional = (value) => `${value}` ? value : ``
     entryMore = (showMoreText) => `
         <a class="table__row clickable">
             <div class="table__col table__col_center">
@@ -67,10 +71,6 @@ class WidgetTemplate {
         </div>`
     entryName = (name) => `<div class="table__col long-line shorter_col">${name}</div>`
     entryValue = (value) => `<div class="table__col long-line shorter_col">${value}</div>`
-    title = (title, subtitle) =>
-        `<h2 class="widget__title" >${title}<span class="widget__subtitle">${subtitle}</span></h2>`
-            ? subtitle
-            : `<h2 class="widget__title" >${title}</h2>`
     linkEntryValue = (text, link) => `<a class="link" href="${link}">${text}</a>`
     codeEntryValue = (value) => `<pre class="code"><code>${value}<code></pre>`
 }
@@ -78,7 +78,7 @@ class WidgetTemplate {
 class GlobalInfoWidget extends Backbone.Marionette.View {
 
     template(data) {
-        return Handlebars.compile(template)(data)
+        return new WidgetTemplate(data).build();
     }
     initialize() {
         this.listLimit = 10;
@@ -88,7 +88,8 @@ class GlobalInfoWidget extends Backbone.Marionette.View {
         return {
             items: items.slice(0, this.listLimit),
             overLimit: items.length > this.listLimit,
-            title: 'QA Info & Warnings'
+            title: 'QA Info & Warnings',
+            subtitle: `${items.length} items total`
         };
     }
 }
